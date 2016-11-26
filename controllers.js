72198764c2dc6d925188865c6dfa86c70dfc17ba
Avin Vij <avinvij26@gmail.com>
2016-11-26 11:17:39 -0800

    data into table

--- controllers.js
+++ controllers.js
@@ -16,10 +16,15 @@ weatherApp.config(function($routeProvider , $mdThemingProvider) {
 });
 
 
-controllers.citycontroller = function($scope , $http){
+controllers.citycontroller = function($scope, $rootScope , $http){
+
+	$rootScope.progresshidden = false;
+	$rootScope.circularprogress = false;
 
 	$scope.getcity = function(){
+		$rootScope.circularprogress = true;
 		$http.get("http://gd.geobytes.com/AutoCompleteCity?q="+$scope.city+"&filter=IN").then(function(response){
+			$rootScope.circularprogress = false;
 			$scope.cities = response.data;
 		});
 	}
@@ -34,15 +39,22 @@ controllers.citycontroller = function($scope , $http){
 	}
 
 	$scope.getweather = function(latitude,longitude){
+		$rootScope.progresshidden = true;
+		$rootScope.fontcolor = "black";	
 		$http.get("https://api.darksky.net/forecast/5f0242b07ca21439da6d1335a945ac0d/"+latitude+","+longitude).
 		then(function(response){
+			$rootScope.progresshidden = false;
 			$scope.temprature = response.data.currently.temperature;
+			$scope.timezone = response.data.timezone;
+			$scope.humidity = response.data.currently.humidity;
+			$scope.windspeed = response.data.currently.windSpeed;
 			var icon = response.data.currently.icon;
 			if(icon == "clear-day")
 				$scope.icoimg = "http://www.zastavki.com/pictures/originals/2014/Nature___Seasons___Spring_Clear_day_in_spring_field_067763_.jpg";
-			else if(icon=="clear-night")
+			else if(icon=="clear-night"){
 				$scope.icoimg = "http://orig11.deviantart.net/81b0/f/2013/353/5/b/on_a_clear_night_sky__background__by_oakfur422-d6yl3xc.png";
-			else if(icon=="rain")
+				$rootScope.fontcolor = "white";
+			}else if(icon=="rain")
 				$scope.icoimg = "http://il8.picdn.net/shutterstock/videos/7770643/thumb/1.jpg";
 			else if(icon=="snow")
 				$scope.icoimg = "https://image.freepik.com/free-photo/christmas-background-with-snow_1048-3135.jpg";
@@ -58,6 +70,7 @@ controllers.citycontroller = function($scope , $http){
 				$scope.icoimg = "http://www.weather.polischrysochous.net/images/day/partlycloudy.jpg";
 			
 		});
+
 	}
 }
 
